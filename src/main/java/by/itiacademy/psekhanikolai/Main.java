791a841d3217cde151534cf5e3b3b029a8db1c25
Nikolai <n91056379@gmail.com>
2024-12-30 15:23:14 +0300

    Initial commit

---
+++ src/main/java/by/itiacademy/psekhanikolai/Main.java
@@ -0,0 +1,15 @@
+package by.itiacademy.psekhanikolai;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> towns = new ArrayList<>();
+        towns.add("Linz");
+        towns.add("Graz");
+        towns.add("Wien");
+        towns.add("Salzburg");
+        towns.add("Bergen");
+        System.out.println(towns);
+    }
+}
