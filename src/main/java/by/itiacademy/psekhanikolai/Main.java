1a79aad9aad7659fc2e6d9209a0804edf4352c50
Nikolai <n91056379@gmail.com>
2024-12-30 15:42:36 +0300

    add method townMax

--- src/main/java/by/itiacademy/psekhanikolai/Main.java
+++ src/main/java/by/itiacademy/psekhanikolai/Main.java
@@ -13,5 +13,15 @@ public class Main {
         System.out.println(towns);
         towns.set(1, "Milan");
         System.out.println(towns);
+        int maxLength = towns.get(0).length();
+        String townMax = towns.get(0);
+        for (String town : towns) {
+            if (maxLength < town.length()) {
+                maxLength= town.length();
+               townMax = town;
+
+            }
+        }
+        System.out.println(townMax);
     }
 }
