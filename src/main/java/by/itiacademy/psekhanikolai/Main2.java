a4cc4fc5a85cad612064dfb4d720c9fb2cfa1acc
Nikolai <n91056379@gmail.com>
2025-01-06 15:14:22 +0300

    add new Main2.java

---
+++ src/main/java/by/itiacademy/psekhanikolai/Main2.java
@@ -0,0 +1,15 @@
+package by.itiacademy.psekhanikolai;
+
+import java.util.ArrayList;
+
+public class Main2 {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Niko");
+        names.add("Alex");
+        names.add("Vladimir");
+        names.add("Kristian");
+        names.add("Marian");
+        System.out.println(names);
+    }
+}
